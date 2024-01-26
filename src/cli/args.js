646ef5d6d40ae9a0962179b3d0503bd6155151cd
Andrey Loginov <andrey12q112@gmail.com>
2024-01-27 02:45:55 +0300

    feat: add cli solutions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv;
+    const result = args.reduce((result, arg, i) => {
+        if (arg.startsWith('--')) {
+            return [
+                ...result,
+                `${arg.slice(2)} is ${args[i+1]}`
+            ];
+        }
+        return result;
+    }, []);
+    console.log(result.join(', '));
 };
 
 parseArgs();
