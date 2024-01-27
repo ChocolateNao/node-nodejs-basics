5bbb26c3f5f299799e88a3b0b65ee414241011ea
Andrey Loginov <andrey12q112@gmail.com>
2024-01-28 02:09:56 +0300

    feat: add child process solution

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,17 @@
+import { fileURLToPath } from 'url';
+import cp from 'child_process';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const file = path.join(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcess = cp.spawn('node', [file, ...args]);
+
+    process.stdin.pipe(childProcess.stdin);
+    childProcess.stdout.pipe(process.stdout);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['amogus', 'sus']);
