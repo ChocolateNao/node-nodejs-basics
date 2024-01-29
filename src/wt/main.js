186ff1ba1d81340da18a6e8f0aed4cf9267dcb69
Andrey Loginov <andrey12q112@gmail.com>
2024-01-29 23:52:24 +0300

    feat: add wt solution

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import os from 'os';
+import path from 'path';
+import { Worker } from 'worker_threads';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const workerFile = path.join(__dirname, 'worker.js');
+
 const performCalculations = async () => {
-    // Write your code here
+    const cpus = os.cpus().length;
+    const results = [];
+
+    const workerTask = (data) => {
+        return new Promise((resolve) => {
+            const worker = new Worker(workerFile, { workerData: data });
+
+            worker.on("message", (value) => {
+                const data = { status: "resolved", data: value };
+                resolve(data);
+            });
+
+            worker.on("error", () => {
+                const data = { status: "error", data: null };
+                resolve(data);
+            });
+        });
+    }
+
+    for (let i = 0; i < cpus; i++) {
+        results.push(workerTask(i + 10));
+    }
+
+    Promise.all(results).then((value) => console.log(value));
 };
 
 await performCalculations();
